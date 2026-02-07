c547701b40a601b9339f5abdb56d6d80faef1b3a
Ginteight <linluc2008@gmail.com>
2026-02-07 13:18:43 +0100

    Uncomment IA imports and initialize AI players

--- ChessTerm-wip-visualization/ChessTerm-wip-visualization/main.py
+++ ChessTerm-wip-visualization/ChessTerm-wip-visualization/main.py
@@ -1,8 +1,6 @@
 from chess import *
 from canvas_tkinter import *
-#import ... as IA1
-#import ... as IA2
-
+from ia import *
 """
 Décommentez les imports pour y mettre votre fichier d'IA
 """
@@ -11,10 +9,14 @@ board = Board()
 root = Tk()
 root.title("Echecs")
 
+a = 234 #233 c'est nul, je préfère 234
+
 """
 Rajoutez le nom de votre fichier pour jouer à votre Jeu et en entrée de la fonction classe Chess_UI
 """
+ia1 = alpha_beta(board, a, -float('inf'), float('inf'), True)
+ia2 = alpha_beta(board, a, -float('inf'), float('inf'), False)
 
-c = Chess_UI(root, board, None, None)
+c = Chess_UI(root, board, ia1, ia2)
 
 root.mainloop()
